9ff4866fc1f29941d93acf870a76a9cddeb2923f
Kacper Kuczewski <kucza06@wp.pl>
2019-05-26 20:20:20 +0200

    husky test

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        alt="elo123"
-      >
-        Learn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn
-        ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn
-        ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn React
-      </a>
-    </header>
-  </div>
-);
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         alt="elo123"
+//       >
+//         Learn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn
+//         ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn
+//         ReactLearn ReactLearn ReactLearn ReactLearn ReactLearn React
+//       </a>
+//     </header>
+//   </div>
+// );
+
+class App extends Component {
+  render() {
+    return (
+      <div classname="App">
+        <div classname="App-header">
+          <img src="{logo}" classname="App-logo" alt="logo">
+          <h2>Welcome to React</h2>
+        </div>
+        <p classname="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
