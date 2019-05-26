cd4fc78bf0ab12bc60c76687cab814a5d35f8603
Kacper Kuczewski <kucza06@wp.pl>
2019-05-26 20:22:37 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div classname="App">
-        <div classname="App-header">
-          <img src="{logo}" classname="App-logo" alt="logo">
+      <div className="App">
+        <div className="App-header">
+          <img src="{logo}" className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p classname="App-intro">
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
